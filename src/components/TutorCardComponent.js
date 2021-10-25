e5a39bfd0efe4c2b3c6d435e21a10092272e1546
Vincent Thomason <p.vincent.thomason@gmail.com>
2021-10-25 12:22:56 -0400

    Add tutor data

--- src/components/TutorCardComponent.js
+++ src/components/TutorCardComponent.js
@@ -11,16 +11,24 @@ import {
 import ratings from "../shared/ratings.json";
 
 class FetchRandomUser extends Component {
-    state = {
-        loading: true,
-        people: [],
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: true,
+            people: [],
+        };
+    }
 
     async componentDidMount() {
         const url = "https://randomuser.me/api/?results=60";
         const response = await fetch(url);
         const data = await response.json();
-        this.setState({ people: data.results, loading: false });
+        let merged = [];
+        for (var i = 0; i < ratings.length; i++) {
+            merged.push({ ...data.results[i], ...ratings[i] });
+        }
+        this.setState({ people: merged, loading: false });
+        console.log(this.state.people);
     }
 
     render() {
@@ -30,6 +38,92 @@ class FetchRandomUser extends Component {
         if (!this.state.people.length) {
             return <div>Tutor data not found.</div>;
         }
+        var ski = "";
+        const skills = (person) => {
+            if (person.codeSkill.length < 2) {
+                ski = "Skills: " + person.codeSkill;
+                return ski;
+            }
+            if (person.codeSkill.length === 2) {
+                ski =
+                    "Skills: " +
+                    person.codeSkill[0] +
+                    ", " +
+                    person.codeSkill[1];
+                return ski;
+            }
+            if (person.codeSkill.length === 3) {
+                ski =
+                    "Skills: " +
+                    person.codeSkill[0] +
+                    ", " +
+                    person.codeSkill[1] +
+                    ", " +
+                    person.codeSkill[2];
+                return ski;
+            } else {
+                ski = "Skills: " + person.codeSkill;
+                return ski;
+            }
+        };
+
+        const stars = (rating) => {
+            if (rating == 1) {
+                return (
+                    <>
+                        <i className="fa fa-star"></i>
+                        <i class="fa fa-star-o" aria-hidden="true"></i>
+                        <i class="fa fa-star-o" aria-hidden="true"></i>
+                        <i class="fa fa-star-o" aria-hidden="true"></i>
+                        <i class="fa fa-star-o" aria-hidden="true"></i>
+                    </>
+                );
+            }
+            if (rating == 2) {
+                return (
+                    <>
+                        <i className="fa fa-star"></i>
+                        <i className="fa fa-star"></i>
+                        <i class="fa fa-star-o" aria-hidden="true"></i>
+                        <i class="fa fa-star-o" aria-hidden="true"></i>
+                        <i class="fa fa-star-o" aria-hidden="true"></i>
+                    </>
+                );
+            }
+            if (rating == 3) {
+                return (
+                    <>
+                        <i className="fa fa-star"></i>
+                        <i className="fa fa-star"></i>
+                        <i className="fa fa-star"></i>
+                        <i class="fa fa-star-o" aria-hidden="true"></i>
+                        <i class="fa fa-star-o" aria-hidden="true"></i>
+                    </>
+                );
+            }
+            if (rating == 4) {
+                return (
+                    <>
+                        <i className="fa fa-star"></i>
+                        <i className="fa fa-star"></i>
+                        <i className="fa fa-star"></i>
+                        <i className="fa fa-star"></i>
+                        <i class="fa fa-star-o" aria-hidden="true"></i>
+                    </>
+                );
+            }
+            if (rating == 5) {
+                return (
+                    <>
+                        <i className="fa fa-star"></i>
+                        <i className="fa fa-star"></i>
+                        <i className="fa fa-star"></i>
+                        <i className="fa fa-star"></i>
+                        <i className="fa fa-star"></i>
+                    </>
+                );
+            }
+        };
 
         return (
             <div className="container">
@@ -56,11 +150,10 @@ class FetchRandomUser extends Component {
                                         tag="h6"
                                         className="mb-2 text-muted"
                                     >
-                                        Code Tutor (rating)
+                                        {stars(person.rating)} {person.rating}(
+                                        {person.ratings})
                                     </CardSubtitle>
-                                    <CardText>
-                                        Replace this section with skills?
-                                    </CardText>
+                                    <CardText>{skills(person)}</CardText>
                                     <Button color="primary">Book</Button>
                                 </CardBody>
                             </Card>
